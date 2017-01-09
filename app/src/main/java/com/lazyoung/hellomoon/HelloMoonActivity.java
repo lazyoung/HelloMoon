b05beb8e910cf3de96d5a9664cfefeb4792163ed
lazyoung <lazyoung@gmail.com>
2017-01-10 07:12:01 +0800

    Initial commit

---
+++ app/src/main/java/com/lazyoung/hellomoon/HelloMoonActivity.java
@@ -0,0 +1,14 @@
+package com.lazyoung.hellomoon;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+
+public class HelloMoonActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_moon);
+    }
+}
